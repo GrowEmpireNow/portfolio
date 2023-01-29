a0b2be39da8352ced676792dbc5c0e04956c7944
harelpanker <harelpanker@gmail.com>
2023-01-29 08:43:44 +0100

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html dir='ltr' lang='en'>
+      <Head>
+        <link
+          href='https://api.fontshare.com/v2/css?f[]=general-sans@1,2&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body className='antialiased font-general text-slate-50 bg-gradient-to-br from-[#141E30] to-[#243B55]'>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
