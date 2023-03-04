583cda503accae5232fc11ff25f3e550ec207e4f
harelpanker <harelpanker@gmail.com>
2023-03-04 12:04:40 +0100

    hero text change

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -24,19 +24,24 @@ const Hero: FC<HeroProps> = (props) => {
           <div className='flex flex-col space-y-3 text-base md:text-xl font-medium text-rose-100/90 mt-5 md:mt-9'>
             <p className='align-middle'>
               Tired of struggling with marketing on your own? want to take your
-              startup to the next level, but don&#39;t know where to start?
+              startup to the next level, but don&apos;t know where to start?
             </p>
+            <p className='align-middle'>
+              EJump into our startup marketing blog Empire Now by Danny Lev.
+            </p>
+            <p className='align-middle'>Need more?</p>
             <p className='align-middle'>
               Empire Now offers CMO as a Service to help startups build out
               their marketing. Our team will create a tailored, data-driven
               marketing strategy, including top-down and bottom-up, PLG, ABM,
-              inbound and outbound funnels. We&#39;ll help with storytelling,
+              inbound and outbound funnels. We&apos;ll help with storytelling,
               messaging, content, PR and social media management, to increase
-              brand awareness, engagement, and differentiation.
+              brand awareness, engagement, and differentiation. All in-house, in
+              the comfort of your own office.
             </p>
             <p className='align-middle'>
-              Don&#39;t let marketing hold you back any longer. Contact us for
-              comprehensive marketing support, starting today. That&#39;s how
+              Don&apos;t let marketing hold you back any longer. Contact us for
+              comprehensive marketing support, starting today. That&apos;s how
               you build an empire 💪
             </p>
           </div>
